400cbbfc2fbe1cb803bcc97ee756c65c3ef05b54
udogaL2 <57914563+udogaL2@users.noreply.github.com>
2023-09-27 19:51:29 +0200

    Исправлена четвертая задача и сделаны 5-8

--- lab2/Main.java
+++ lab2/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
     public static void main(String[] arg)
     {
-        System.out.println(maxSubarraySum(new int[]{1, 2, -3, 4, -1, 2, 1, -5, 4}));
+        System.out.println((Arrays.deepToString(rotateMatrixCounterclockwise(new int[][]{{1, 2, 3}, {4, 5, 6}}))));
     }
 
+    // #1
     public static String getMaxUnicSubstr(String inputString)
     {
         String usedChars = "";
@@ -40,6 +41,7 @@ public class Main {
         return inputString.substring(maxStart, maxStart + maxLength);
     }
 
+    // #2
     public static int[] getMergeWithSort(int[] array1, int[] array2)
     {
         int[] arrayToReturn = new int[array1.length + array2.length];
@@ -61,6 +63,7 @@ public class Main {
         return arrayToReturn;
     }
 
+    // #3
     public static int maxSubarraySum(int[] arr) {
         int maxSum = arr[0];
         int currentSum = arr[0];
@@ -73,15 +76,93 @@ public class Main {
         return maxSum;
     }
 
-    public static int[][] rotateMatrix(int[][] matrix)
+    // #4
+    public static int[][] rotateMatrixClockwise(int[][] matrix)
     {
-        int[][] newMatrix = new int[matrix[0].length][matrix.length];
+        int rows = matrix.length;
+        int cols = matrix[0].length;
 
-        for(int i = 0; i < matrix.length; i++)
+        int[][] newMatrix = new int[cols][rows];
+
+        for(int i = 0; i < rows; i++)
         {
-            for (int j = 0; j < matrix[0].length; j++)
+            for (int j = 0; j < cols; j++)
             {
-                newMatrix[j][i] = matrix[i][j];
+                newMatrix[j][rows - 1 - i] = matrix[i][j];
+            }
+        }
+
+        return newMatrix;
+    }
+
+    // #5
+    public static int[] getPair(int[] numbers, int target)
+    {
+        for(int i = 0; i < numbers.length - 1; i++)
+        {
+            for(int j = i + 1; j < numbers.length; j++)
+            {
+                if(numbers[i] + numbers[j] == target)
+                {
+                    return new int[]{numbers[i], numbers[j]};
+                }
+            }
+        }
+        return null;
+    }
+
+    // #6
+    public static int getSumOfMatrix(int[][] matrix)
+    {
+        int sum = 0;
+
+        for(int[] row : matrix)
+        {
+            for(int element: row)
+            {
+                sum += element;
+            }
+        }
+
+        return sum;
+    }
+
+    // #7
+    public static int[] getMaxElementInEachRowOfMatrix(int[][] matrix)
+    {
+        int[] arrayToReturn = new int[matrix.length];
+        int index = 0;
+
+        for(int[] row : matrix)
+        {
+            int maxElement = row[0];
+
+            for(int element : row)
+            {
+                if(element > maxElement)
+                {
+                    maxElement = element;
+                }
+            }
+            arrayToReturn[index] = maxElement;
+            index++;
+        }
+
+        return arrayToReturn;
+    }
+
+    public static int[][] rotateMatrixCounterclockwise(int[][] matrix)
+    {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[][] newMatrix = new int[cols][rows];
+
+        for(int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                newMatrix[cols - 1 - j][i] = matrix[i][j];
             }
         }
 
